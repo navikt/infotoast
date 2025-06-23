970598f9e15251d5374fcc2acc1fd4f7871e37e0
Helene Arnesen <helene.arnesen@nav.no>
2025-06-23 13:00:04 +0200

    chore: spring init

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "no.nav.tsm"
+version = "0.0.2"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+
+repositories {
+    mavenCentral()
+    maven(url = "https://packages.confluent.io/maven/")
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    bootJar {
+        archiveFileName = "app.jar"
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("skipped", "failed")
+            showStackTraces = true
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
+    }
+}
